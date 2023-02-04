58b05e145e8bc22b36f1b246b590c84f45cc66f2
amar das <amarkumar13012000@gmail.com>
2023-02-04 10:36:56 +0530

    updated

--- app/src/main/java/amar/das/acbook/utility/MyProjectUtility.java
+++ app/src/main/java/amar/das/acbook/utility/MyProjectUtility.java
@@ -6,6 +6,6 @@ import java.util.Date;
 public class MyProjectUtility {
     public static int get24hrCurrentTimeRemoveColon() {
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");//capital HH stands for 24hr time format
-       return  Integer.parseInt(sdf.format(new Date()).replaceAll("[:]", ""));//convert 01:30:55 to 13055
+       return  Integer.parseInt(sdf.format(new Date()).replaceAll("[:]", ""));//convert 01:30:55 to 13055 by parsing to INTEGER
     }
 }
